51d33c8ce4f64c60418f5ab490c0d9efded39d01
Dmitry Chubarov <dime@twowls.org>
2025-04-10 22:11:39 +0400

    Play with theme colors.

--- apps/platform/src/app/page.tsx
+++ apps/platform/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { TypographyH1 } from '@/components/ui/typography';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-4">
       <TypographyH1>Page content</TypographyH1>
+      <Button variant="destructive" size="lg" className="mt-5">
+        Danger!
+      </Button>
     </div>
   );
 }
