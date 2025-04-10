5a8b00a9cd720e91d026cab2a6842923020fef55
Dmitry Chubarov <dime@twowls.org>
2025-04-10 20:10:45 +0400

    Add Inter font.

--- apps/platform/src/components/button.tsx
+++ apps/platform/src/components/button.tsx
@@ -1,6 +1,6 @@
-import * as Headless from '@headlessui/react';
-import clsx from 'clsx';
 import React, { forwardRef } from 'react';
+import clsx from 'clsx';
+import * as Headless from '@headlessui/react';
 import { Link } from './link';
 
 const styles = {
